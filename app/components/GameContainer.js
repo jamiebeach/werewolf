1322bd2cf42cda558bbbaef599bc00e3ae2908af
jeunlee <junniesu@gmail.com>
2017-02-10 13:15:13 -0500

    once you have joined a game, refresh and leaving are prompted

--- app/components/GameContainer.js
+++ app/components/GameContainer.js
@@ -3,13 +3,27 @@ import ChatContainer from './ChatContainer';
 import JoinGame from './JoinGame';
 import {connect} from 'react-redux';
 
-const GameContainer = props => {
-  return (
-    <div>
-      {props.player.joined ? <ChatContainer /> : <JoinGame />}
-      }
-    </div>
-  )
+export class GameContainer extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  componentDidMount() {
+    window.onbeforeunload = () => {
+      return 'You may forfeit any current games if you leave.';
+      // this makes the browser ask you if you really want to leave or reload the page
+      //no custom message is possible....
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.player.joined ? <ChatContainer /> : <JoinGame />}
+        }
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
