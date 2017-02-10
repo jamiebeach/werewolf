b60a12d622af194eb6594ea4ec3308d0555798c2
fy62 <felicia.yau@gmail.com>
2017-02-10 13:44:12 -0500

    componentdidmount stuff

--- app/components/GameContainer.js
+++ app/components/GameContainer.js
@@ -2,37 +2,51 @@ import React, {Component} from 'react';
 import ChatContainer from './ChatContainer';
 import JoinGame from './JoinGame';
 import {connect} from 'react-redux';
-import {updateGameActions} from '../reducers/game';
+import {updateGameActions, updatePlayer, joinGame} from '../reducers/game';
 
 export class GameContainer extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    console.log(props.auth);
+
   }
 
-  ComponentDidUpdate (prevProps, prevState){
-    if(!prevProps.player.uid && this.props.player.uid) {
-      console.log('****** inside if Statement *********')
-      const rosterPlayer = firebase.database().ref(`games/${gameId}/roster/${this.props.player.uid}`).once('value')
+  // ComponentDidUpdate (prevProps, prevState){
+  //   console.log("changed: ", prevProps, this.props);
+  //   if(!prevProps.auth && this.props.auth) {
+  //     console.log('****** inside if Statement *********')
+
+  //   }
+  // }
+
+  componentDidMount (){
+    console.log('mounted!!!!', this.props)
+    const rosterPlayer = firebase.database().ref(`games/${this.props.params.id.gameId}/roster/${this.props.auth.uid}`).once('value')
       rosterPlayer.then(res => {
-        if(res) updateGameActions();
+        console.log(res, res.name)
+        if(res) {
+          console.log('hello??')
+          updatePlayer({name: res.name});
+          updateGameActions();
+        }
       })
-    }
   }
 
   render () {
     return (
       <div>
-        {console.log('inside gameContainer, player = ', this.props.player)}
+        {/*console.log('inside gameContainer, player = ', this.props.player)*/}
         {this.props.player.joined ? <ChatContainer /> : <JoinGame />}
         }
       </div>
     )
   }
-  
+
 }
 
 const mapStateToProps = state => {
   return {
+    auth: state.auth,
     player: state.game.player,
     games: state.game.games
   };
